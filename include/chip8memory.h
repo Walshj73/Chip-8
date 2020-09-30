e8fcc3218a41cf8ebc01de681f54d5802e572744
Jason <walshjason@live.com>
2020-09-30 23:45:36 +0100

    Added the stack and registers.

--- include/chip8memory.h
+++ include/chip8memory.h
@@ -2,11 +2,12 @@
 #define CHIP8MEMORY_H
 
 #include "config.h"
-struct chip8_memory {
+struct chip8_memory
+{
     unsigned char memory[CHIP8_MEMORY_SIZE];
 };
 
-void chip8_memory_set (struct chip8_memory* memory, int index, unsigned char val);
-unsigned char chip8_memory_get (struct chip8_memory* memory, int index);
+void chip8_memory_set(struct chip8_memory *memory, int index, unsigned char val);
+unsigned char chip8_memory_get(struct chip8_memory *memory, int index);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -2,27 +2,34 @@
 #include "SDL2/SDL.h"
 #include "chip8.h"
 
-int main (int argc, char** argv) {
+int main(int argc, char **argv)
+{
 
     struct chip8 chip8;
-    chip8_memory_set(&chip8.memory, 0x400, 'z');
-    printf("%c\n", chip8_memory_get(&chip8.memory, 50));
+    chip8.registers.SP = 0;
+    chip8_stack_push(&chip8, 0xff);
+    chip8_stack_push(&chip8, 0xaa);
+
+    printf("%x\n", chip8_stack_pop(&chip8));
+    printf("%x\n", chip8_stack_pop(&chip8));
 
     SDL_Init(SDL_INIT_EVERYTHING);
     SDL_Window *window = SDL_CreateWindow(
         EMULATOR_WINDOW_TITLE,
         SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
-        CHIP8_WIDTH * CHIP8_WINDOW_MULTIPLIER, 
-        CHIP8_HEIGHT * CHIP8_WINDOW_MULTIPLIER, 
-        SDL_WINDOW_SHOWN
-    );
+        CHIP8_WIDTH * CHIP8_WINDOW_MULTIPLIER,
+        CHIP8_HEIGHT * CHIP8_WINDOW_MULTIPLIER,
+        SDL_WINDOW_SHOWN);
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_TEXTUREACCESS_TARGET);
-    while (1) {
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_TEXTUREACCESS_TARGET);
+    while (1)
+    {
         SDL_Event event;
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+            {
                 goto out;
             }
         }
